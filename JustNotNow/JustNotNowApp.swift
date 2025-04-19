36eed56da33228f782c2348342fcb3673baf7d19
Holger Hinzberg <holger@hinzberg.de>
2025-04-19 11:03:49 +0200

    Initial Commit

---
+++ JustNotNow/JustNotNowApp.swift
@@ -0,0 +1,17 @@
+//
+//  JustNotNowApp.swift
+//  JustNotNow
+//
+//  Created by Holger Hinzberg on 19.04.25.
+//
+
+import SwiftUI
+
+@main
+struct JustNotNowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JustNotNowTests/JustNotNowTests.swift
@@ -0,0 +1,17 @@
+//
+//  JustNotNowTests.swift
+//  JustNotNowTests
+//
+//  Created by Holger Hinzberg on 19.04.25.
+//
+
+import Testing
+@testable import JustNotNow
+
+struct JustNotNowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
